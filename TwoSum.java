d593aff32ce4cb68b154dbc39f967ce34cdb2c41
Sudhakar-812 <sudhakarchinnathambi812@gmail.com>
2023-07-03 08:14:53 +0530

    Twosum Solution

---
+++ TwoSum.java
@@ -0,0 +1,16 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0;
+        int j = 0;
+        int a[]=new int[2];
+        for(i=0; i<nums.length; i++){
+            for(j=i+1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    a[0]=i;
+                    a[1]=j;
+                }
+            }
+        }
+        return a;
+    }
+}
